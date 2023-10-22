f9d362c1013384e8f6b8f0005f631b0c3e13bba6
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-22 21:20:14 +0300

    added changes in failes

--- js/passwordGenerator.js
+++ js/passwordGenerator.js
@@ -1,13 +1,13 @@
-const generateBtn = document.querySelecto(".generator__btn__generate");
+const generateBtn = document.querySelector(".generator__btn_generate");
 const copyBtn = document.querySelector(".generator__btn_copy");
-const randomOassword = document.querySelector(".generator__password");
-const passwordLength = document.querySelector(".length__velue");
+const randomPassword = document.querySelector(".generator__password");
+const passwordLength = document.querySelector(".length__value");
 const isUppercase = document.querySelector("#uppercase");
 const isLowercase = document.querySelector("#lowercase");
 const isNumber = document.querySelector("#number");
 const isSymbol = document.querySelector("#symbol");
-const notification = document.querySelector("generator__notification");
-const checkboxes = document.querySelector('input[type="checkbox"]');
+const notification = document.querySelector(".generator__notification");
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
 generateBtn.addEventListener("click", handleGeneratePassword);
 copyBtn.addEventListener("click", copyPassword);
@@ -54,7 +54,7 @@ function handleGeneratePassword() {
 }
 
 function generatePassword(lower, upper, number, symbol, length) {
-  let generatePassword = "";
+  let generatedPassword = "";
 
   const typesCount = lower + upper + number + symbol;
   const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
@@ -72,7 +72,8 @@ function generatePassword(lower, upper, number, symbol, length) {
       generatedPassword += randomFunc[funcName]();
     });
   }
-  const result = generatePassword.slice(0, length);
+
+  const result = generatedPassword.slice(0, length);
 
   return result;
 }
